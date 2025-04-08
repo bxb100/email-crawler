1cf67fd34f08715d261c6f08795272729894ace3
Lemon <bxb100@outlook.com>
2025-04-08 10:53:47 +0800

    chore: sundry

--- index.js
+++ index.js
@@ -8,133 +8,136 @@ const xlsx = require('node-xlsx').default;
 
 (async () => {
 
-  const filename = "兰州交通大学";
-  const url = "https://xnyxy.lzjtu.edu.cn/szdw.htm";
-  const selector = ".right table:nth-child(2) a";
-  const button = "middle";
-  const next_page_element = null;
+	const filename = "储运与建筑工程学院";
+	const url = "https://cj.upc.edu.cn/szdw/list.htm";
+	const selector = "#teacherBox li";
+	const button = "middle";
+	const next_page_element = null;
 
-  const client = await createClient()
-    .on('error', err => console.log('Redis Client Error', err))
-    .connect();
+	const client = await createClient()
+		.on('error', err => console.log('Redis Client Error', err))
+		.connect();
 
-  // puppeteer.use(StealthPlugin());
+	// puppeteer.use(StealthPlugin());
 
-  const browser = await puppeteer.launch({
-    headless: false,
-    executablePath: puppeteer.executablePath('chrome'),
-    ignoreHTTPSErrors: true,
-    args: [
-      "--disable-gpu",
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      '--disable-infobars',
-      '--window-position=0,0',
-      '--ignore-certificate-errors',
-      '--ignore-certificate-errors',
-      '--ignore-certificate-errors-spki-list',
-      "--disable-dev-shm-usage",
-      "--disable-blink-features=AutomationControlled",
-    ],
-  },);
+	const browser = await puppeteer.launch({
+		headless: true,
+		executablePath: puppeteer.executablePath('chrome'),
+		ignoreHTTPSErrors: true,
+		args: [ "--disable-gpu", "--no-sandbox", "--disable-setuid-sandbox", '--disable-infobars', '--window-position=0,0', '--ignore-certificate-errors', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', "--disable-dev-shm-usage", "--disable-blink-features=AutomationControlled", ],
+	},);
 
-  let homepage_context = await browser.createBrowserContext();
-  const page = await homepage_context.newPage();
-  await page_inject(page);
+	let homepage_context = await browser.createBrowserContext();
+	const page = await homepage_context.newPage();
+	await page_inject(page);
 
-  homepage_context.on('targetcreated', async e => {
-    console.log(e.url());
+	homepage_context.on('targetcreated', async e => {
+		console.log(e.url());
 
-    if (e.url()) {
-      if (e.url().startsWith(url) || e.url() === 'about:blank') {
-        try {
-          const page1 = await e.page();
-          await page1.waitForNetworkIdle();
-          await page1.close();
-        } catch (e) {
-          // console.error(e);
-        }
-        // 一般来说只有 # 跳转才会出现当前页面开头一致
-        await client.del("latest")
-        return;
-      }
-      const page1 = await e.page();
-      if (page1) {
-        try {
-          const latest_name = await client.get("latest");
-          await client.zAdd("mapped", {
-            score: 0, value: JSON.stringify({ url: e.url(), name: latest_name })
-          });
-          await client.del("latest")
-          await page1.close();
-        } catch (e) {
-          console.error(e);
-        }
+		if (e.url()) {
+			if (e.url().startsWith(url) || e.url() === 'about:blank') {
+				try {
+					const page1 = await e.page();
+					await page1.waitForNetworkIdle();
+					await page1.close();
+				} catch (e) {
+					// console.error(e);
+				}
+				// 一般来说只有 # 跳转才会出现当前页面开头一致
+				await client.del("latest")
+				return;
+			}
+			const page1 = await e.page();
+			if (page1) {
+				try {
+					const latest_name = await client.get("latest");
+					await client.zAdd("mapped", {
+						score: 0, value: JSON.stringify({ url: e.url(), name: latest_name })
+					});
+					await client.del("latest")
+					await page1.close();
+				} catch (e) {
+					console.error(e);
+				}
 
-      }
-    }
-  });
+			}
+		}
+	});
 
-  await start({ page, client, url, selector, button, next_page_element });
-  await homepage_context.close();
+	await start({ page, client, url, selector, button, next_page_element });
+	await homepage_context.close();
 
-  await build_data(browser, client, filename);
+	await build_data(browser, client, filename);
 
-  await browser.close();
-  await client.disconnect();
+	await browser.close();
+	await client.disconnect();
 
 })()
-  .then(() => console.log('Script complete!'))
-  .catch((err) => console.error('Error running script' + err))
-  .finally(() => process.exit());
+	.then(() => console.log('Script complete!'))
+	.catch((err) => console.error('Error running script' + err))
+	.finally(() => process.exit());
 
 async function build_data(browser, client, filename) {
-  const mapping = await client.zRange("mapped", 0, -1)
+	const email_regex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/gm
+	const users = [];
 
-  const email_regex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/gm
+	const mapping = await client.zRange("mapped", 0, -1)
+	let context = await browser.createBrowserContext();
 
-  const users = [];
-  let context = await browser.createBrowserContext();
+	let promise_tasks = [];
+	let i = 0;
 
-  let promise_tasks = [];
-  let i = 0;
-  for (let map of mapping) {
-    const user = [];
-    users.push(user);
 
-    const { url, name } = JSON.parse(map);
-    user.push(name);
+	for (let map of mapping) {
+		const user = [];
+		users.push(user);
 
-    promise_tasks.push(context.newPage().then(async page => {
-      page.setDefaultNavigationTimeout(0);
-      page.setDefaultTimeout(0);
-      await page_inject(page);
+		const { url, name } = JSON.parse(map);
+		user.push(name);
 
-      await page.goto(url, { waitUntil: 'networkidle0', timeout: 13000 });
-      try {
-        await new Promise(resolve => setTimeout(resolve, 3000));
+		promise_tasks.push(new Promise(async (resolve) => {
+			let content = await client.get(url);
+			if (!content) {
+				try {
+					const page = await context.newPage();
+					page.setDefaultNavigationTimeout(0);
+					page.setDefaultTimeout(0);
+					await page_inject(page);
+					await page.goto(url, { waitUntil: 'networkidle0', timeout: 13000 });
+					await new Promise(resolve => setTimeout(resolve, 3000));
 
-        const md = await fetchAndProcessPage(page);
-        await client.set(url, md);
+					content = await page.$eval("body", el => el.innerText);
+					const md = await fetchAndProcessPage(page);
+					await client.set(url, md);
+					await page.close();
+				} catch (e) {
+					console.error(e);
+				}
+			}
 
-        const content = await page.$eval("body", el => el.innerText);
+			if (content) {
+				const email = content.match(email_regex)
+				if (email && email.length > 0) {
+					user.push(email[0]);
+				} else {
+					user.push(null);
+				}
+				const department = content.match(/当前位置：首页\s*(.+)/gm);
+				if (department && department.length > 0) {
+					user.push(department[0].replace(/当前位置：首页/g, "").trim());
+				}else {
+					user.push(null);
+				}
 
-        await page.close();
-        if (content) {
-          const email = content.match(email_regex)
-          if (email && email.length > 0) {
-            user.push(email[0]);
-            user.push(url);
-            return;
-          }
-        }
-      } catch (e) {
-        console.error(e, url);
-      }
-
-      user.push(null);
-      user.push(url);
-    }));
+				user.push(url);
+				resolve()
+				return;
+			}
+			user.push(null);
+			user.push(null);
+			user.push(url);
+			resolve()
+		}))
 
     if (++i % 20 === 0) {
       await Promise.all(promise_tasks);
@@ -151,46 +154,46 @@ async function build_data(browser, client, filename) {
 }
 
 async function page_inject(page) {
-  await page.evaluateOnNewDocument(() => {
-    delete navigator.__proto__.webdriver;
-  });
-  await page.evaluateOnNewDocument(() => {
-    const newProto = navigator.__proto__;
-    delete newProto.webdriver;
-    navigator.__proto__ = newProto;
-  });
-  await page.evaluateOnNewDocument(() => {
-    [ ...document.getElementsByTagName('img') ].forEach(i => i.onerror = null);
-  });
-  page.once('dialog', dialog => {
-    console.log(dialog.message());
-    console.log("Dismissing " + dialog.type());
-    dialog.dismiss();
-  });
-  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-  await page.setRequestInterception(true);
-  page.on('request', (req) => {
-    if (req.resourceType() === 'font' || req.resourceType() === 'image') {
-      req.abort();
-    } else {
-      req.continue();
-    }
-  });
-  return page;
+	await page.evaluateOnNewDocument(() => {
+		delete navigator.__proto__.webdriver;
+	});
+	await page.evaluateOnNewDocument(() => {
+		const newProto = navigator.__proto__;
+		delete newProto.webdriver;
+		navigator.__proto__ = newProto;
+	});
+	await page.evaluateOnNewDocument(() => {
+		[ ...document.getElementsByTagName('img') ].forEach(i => i.onerror = null);
+	});
+	page.once('dialog', dialog => {
+		console.log(dialog.message());
+		console.log("Dismissing " + dialog.type());
+		dialog.dismiss();
+	});
+	await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+	await page.setRequestInterception(true);
+	page.on('request', (req) => {
+		if (req.resourceType() === 'font' || req.resourceType() === 'image') {
+			req.abort();
+		} else {
+			req.continue();
+		}
+	});
+	return page;
 }
 
 async function fetchAndProcessPage(page, enableDetailedResponse) {
-  return await page.evaluate((enableDetailedResponse) => {
-    function extractArticleMarkdown() {
-      const readabilityScript = document.createElement('script');
-      readabilityScript.src = 'https://unpkg.com/@mozilla/readability/Readability.js';
-      document.head.appendChild(readabilityScript);
+	return await page.evaluate((enableDetailedResponse) => {
+		function extractArticleMarkdown() {
+			const readabilityScript = document.createElement('script');
+			readabilityScript.src = 'https://unpkg.com/@mozilla/readability/Readability.js';
+			document.head.appendChild(readabilityScript);
 
-      const turndownScript = document.createElement('script');
-      turndownScript.src = 'https://unpkg.com/turndown/dist/turndown.js';
-      document.head.appendChild(turndownScript);
+			const turndownScript = document.createElement('script');
+			turndownScript.src = 'https://unpkg.com/turndown/dist/turndown.js';
+			document.head.appendChild(turndownScript);
 
-      let md = 'no content';
+			let md = 'no content';
 
       // Wait for the libraries to load
       md = Promise.all([
@@ -204,27 +207,27 @@ async function fetchAndProcessPage(page, enableDetailedResponse) {
           nbTopCandidates: 500,
         });
 
-        // Parse the article content
-        const article = reader.parse();
+				// Parse the article content
+				const article = reader.parse();
 
-        // Turndown instance to convert HTML to Markdown
-        const turndownService = new TurndownService();
+				// Turndown instance to convert HTML to Markdown
+				const turndownService = new TurndownService();
 
-        let documentWithoutScripts = document.cloneNode(true);
-        documentWithoutScripts.querySelectorAll('script').forEach((browserItem) => browserItem.remove());
-        documentWithoutScripts.querySelectorAll('style').forEach((browserItem) => browserItem.remove());
-        documentWithoutScripts.querySelectorAll('iframe').forEach((browserItem) => browserItem.remove());
-        documentWithoutScripts.querySelectorAll('noscript').forEach((browserItem) => browserItem.remove());
+				let documentWithoutScripts = document.cloneNode(true);
+				documentWithoutScripts.querySelectorAll('script').forEach((browserItem) => browserItem.remove());
+				documentWithoutScripts.querySelectorAll('style').forEach((browserItem) => browserItem.remove());
+				documentWithoutScripts.querySelectorAll('iframe').forEach((browserItem) => browserItem.remove());
+				documentWithoutScripts.querySelectorAll('noscript').forEach((browserItem) => browserItem.remove());
 
-        // article content to Markdown
-        const markdown = turndownService.turndown(enableDetailedResponse ? documentWithoutScripts : article.content);
+				// article content to Markdown
+				const markdown = turndownService.turndown(enableDetailedResponse ? documentWithoutScripts : article.content);
 
-        return markdown;
-      });
+				return markdown;
+			});
 
-      return md;
-    }
+			return md;
+		}
 
-    return extractArticleMarkdown();
-  }, enableDetailedResponse);
+		return extractArticleMarkdown();
+	}, enableDetailedResponse);
 }
--- start.js
+++ start.js
@@ -2,71 +2,71 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = async ({ page, client, url, selector, button = 'middle', next_page_element }) => {
 
-  await page.goto(url, { waitUntil: 'networkidle0' });
+	await page.goto(url, { waitUntil: 'networkidle0' });
 
-  await handle(page, client, selector, button, null);
-  if (next_page_element) {
-    await handle_next_page(page, client, selector, button, next_page_element);
-  }
+	await handle(page, client, selector, button, null);
+	if (next_page_element) {
+		await handle_next_page(page, client, selector, button, next_page_element);
+	}
 
-  await page.close();
+	await page.close();
 };
 
 
 async function handle_next_page(page, client, selector, button, next_page_element) {
-  const next_page = await page.$(next_page_element)
-  if (next_page) {
-    const innerText = await page.evaluate(() => document.body.innerText);
+	const next_page = await page.$(next_page_element)
+	if (next_page) {
+		const innerText = await page.evaluate(() => document.body.innerText);
 
-    next_page.click();
+		next_page.click();
 
-    try {
-      await handle(page, client, selector, button, innerText);
-      await handle_next_page(page, client, selector, button, next_page_element);
-    } catch (e) {
-      console.log(e.message);
-    }
-  }
+		try {
+			await handle(page, client, selector, button, innerText);
+			await handle_next_page(page, client, selector, button, next_page_element);
+		} catch (e) {
+			console.log(e.message);
+		}
+	}
 }
 
 async function handle(page, client, selector, button, page_hash) {
-  await page.waitForNetworkIdle();
-  await page.waitForSelector(selector);
+	await page.waitForNetworkIdle();
+	await page.waitForSelector(selector);
 
-  if (page_hash) {
-    const innerText = await page.evaluate(() => document.body.innerText);
+	if (page_hash) {
+		const innerText = await page.evaluate(() => document.body.innerText);
 
-    if (page_hash === innerText) {
-      throw new Error('Duplicate page');
-    }
-  }
+		if (page_hash === innerText) {
+			throw new Error('Duplicate page');
+		}
+	}
 
-  const elements = await page.$$(selector)
-  let options = {
-    button
-  };
-  for (let element of elements) {
-    if (await element.isVisible()) {
-      let name = await page.evaluate(el => el.innerText.replace(/(\p{Script=Hani})\s+(?=\p{Script=Hani})/gu, '$1'), element);
-      name = name.trim();
-      if (!name) {
-        continue;
-      }
-      await client.set('latest', name);
-      try {
-        await element.click(options);
-        const now = process.hrtime.bigint();
-        while (await client.get('latest')) {
-          // 10s
-          if (process.hrtime.bigint() - now > 10000000000) {
-            console.log('Timeout', name);
-            break;
-          }
-          await new Promise(resolve => setTimeout(resolve, 300));
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }
+	const elements = await page.$$(selector)
+	let options = {
+		button
+	};
+	for (let element of elements) {
+		if (await element.isVisible()) {
+			let name = await page.evaluate(el => el.innerText.replace(/(\p{Script=Hani})\s+(?=\p{Script=Hani})/gu, '$1'), element);
+			name = name.trim();
+			if (!name) {
+				continue;
+			}
+			await client.set('latest', name);
+			try {
+				await element.click(options);
+				const now = process.hrtime.bigint();
+				while (await client.get('latest')) {
+					// 10s
+					if (process.hrtime.bigint() - now > 10000000000) {
+						console.log('Timeout', name);
+						break;
+					}
+					await new Promise(resolve => setTimeout(resolve, 300));
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}
 }
